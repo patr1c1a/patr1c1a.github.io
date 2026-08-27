acf7f811216bb9c7461fa4cea8194418df75ed99
Patricia E. Miguel <contacto@patriciaemiguel.com>
2026-08-27 12:18:32 -0300

    Implement pending registration creation

--- functions/main.py
+++ functions/main.py
@@ -1,21 +1,54 @@
-# Welcome to Cloud Functions for Firebase for Python!
-# To get started, simply uncomment the below code or create your own.
-# Deploy with `firebase deploy`
-
 from firebase_functions import https_fn
 from firebase_functions.options import set_global_options
-from firebase_admin import initialize_app
+from firebase_admin import initialize_app, firestore
 
-# For cost control, you can set the maximum number of containers that can be
-# running at the same time. This helps mitigate the impact of unexpected
-# traffic spikes by instead downgrading performance. This limit is a per-function
-# limit. You can override the limit for each function using the max_instances
-# parameter in the decorator, e.g. @https_fn.on_request(max_instances=5).
-set_global_options(max_instances=10)
 
-# initialize_app()
-#
-#
-# @https_fn.on_request()
-# def on_request_example(req: https_fn.Request) -> https_fn.Response:
-#     return https_fn.Response("Hello world!")
+set_global_options(
+    region="southamerica-east1",
+    max_instances=1,
+)
+
+initialize_app()
+
+
+@https_fn.on_request()
+def createRegistration(req: https_fn.Request) -> https_fn.Response:
+
+    if req.method != "POST":
+        return https_fn.Response(
+            "Method Not Allowed",
+            status=405,
+        )
+
+    data = req.get_json(silent=True)
+
+    if not data:
+        return https_fn.Response(
+            "Invalid JSON",
+            status=400,
+        )
+
+    product_slug = data.get("product_slug")
+    purchase_option = data.get("purchase_option")
+
+    if not product_slug or not purchase_option:
+        return https_fn.Response(
+            "Missing product_slug or purchase_option",
+            status=400,
+        )
+
+    db = firestore.client()
+
+    registration = {
+        "product_slug": product_slug,
+        "purchase_option": purchase_option,
+        "status": "pending_payment",
+        "created_at": firestore.SERVER_TIMESTAMP,
+    }
+
+    document = db.collection("registrations").add(registration)
+
+    return https_fn.Response(
+        f"Registration created: {document[1].id}",
+        status=201,
+    )
